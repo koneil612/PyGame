2d3baf7a03654ef411f12f993072616d6fd3ccd0
koneil612 <kellis1286@gmail.com>
2016-12-16 14:00:45 -0600

    making the christmas card list

--- Flask/PhonebookFlask.py
+++ Flask/PhonebookFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, redirect
 import mysql.connector
 
 app = Flask("MyApp")
@@ -18,7 +18,9 @@ def home():
     list = cur.fetchall()
     q=request.args.get('q')
     # print list
-    return render_template("phonebook.html", phonebook_list=list, title="Phonebook List", q=q)
+
+    return render_template("phonebook.html", phonebook_list=list, title="Christmas Card List", q=q)
+
 
 @app.route('/new_entry')
 def new_entry():
@@ -30,11 +32,46 @@ def submit_new_entry():
     address=request.form.get('address')
     city=request.form.get('city')
     state=request.form.get('state')
-    zip=request.form.get('state')
-    query= ("insert into phonebook (name, address, city), (state),(zip) values (\"%s\") (\"%s\") (\"%s\") (\"%s\")(\"%s\")" % (name, address, city, state, zip)
+    zip=request.form.get('zip')
+    query= "INSERT INTO phonebook (name, address, city, state,zip) values ('%s','%s','%s','%s','%s')" % (name, address, city, state, zip)
     cur.execute(query)
     conn.commit()
     return render_template("submit_new_entry.html", name=name, address=address, city=city, state=state, zip=zip)
+
+@app.route('/update_entry', methods=['GET'])
+def update_entry():
+    id=request.args.get('id')
+    query = "SELECT id, name, address, city, state, zip FROM phonebook WHERE id=%s" %id
+    cur.execute(query)
+    list = cur.fetchone()
+    id=list[0]
+    name=list[1]
+    address=list[2]
+    city=list[3]
+    state=list[4]
+    zip=list[5]
+    return render_template("update_entry.html", name=name, address=address, city=city, state=state, zip=zip,id=id)
+
+@app.route('/submit_update_entry', methods=['POST', 'GET'])
+def submit_update_entry():
+    id=request.form.get('id')
+    name=request.form.get('name')
+    address=request.form.get('address')
+    city=request.form.get('city')
+    state=request.form.get('state')
+    zip=request.form.get('zip')
+    query= "UPDATE phonebook SET name = '%s', address='%s', city='%s', state='%s', zip='%s' WHERE id = %s" % (name, address, city, state, zip, id)
+    cur.execute(query)
+    conn.commit()
+    return render_template("submit_update_entry.html", name=name)
+
+@app.route("/delete_contact",methods=["GET"])
+def delete_contact():
+    id=request.args.get('id')
+    query = ("DELETE from phonebook where id=%s" % id)
+    cur.execute(query)
+    conn.commit()
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
